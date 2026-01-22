72f77e005fedf82b359d4f8471cd083340b06b37
Ntsakelo <ntsakycodes@gmail.com>
2026-01-23 00:49:29 +0200

    product images paths updated

--- src/data/products.js
+++ src/data/products.js
@@ -1,3 +1,4 @@
+// import appleEarPhones from '../assets/apple_earphone'
 
 
 
@@ -6,14 +7,128 @@
 
 
 
+
+// export const products = [
+//     {
+//         id: "5RKWDRWZ4N",
+//         name: "ASUS ROG Zephyrus G16",
+//         description: "The ASUS ROG Zephyrus G16 gaming laptop is powered by the Intel Core i9 processor and features an RTX 4070 GPU, delivering top-tier gaming and performance. With 16GB of RAM and a 1TB SSD, this laptop is designed for gamers who demand extreme power, speed, and storage. Equipped with a stunning 16-inch display, it's built to handle the most demanding titles and applications with ease.",
+//         image: "/src/assets/asus_laptop_image.png",
+//         price: 34599.99,
+//         rating: 4.5,
+//         color: "Multi",
+//         category: "Laptops"
+//     },
+//     {
+//         id: "BJJFZBBDIA",
+//         name: "Bose QuietComfort 45",
+//         description: "The Bose QuietComfort 45 headphones are engineered for exceptional sound quality and unparalleled noise cancellation. With a 24-hour battery life and comfortable, lightweight design, these headphones deliver premium audio for any environment. Whether on a flight, in the office, or at home, the Bose QC45 blocks out distractions, offering an immersive listening experience.",
+//         image: "/src/assets/bose_headphone_image.png",
+//         price: 329.99,
+//         rating: 4.5,
+//         color: "Black",
+//         category: "Headphones"
+//     },
+//     {
+//         id: "MDI7BWBE0T",
+//         name: "Samsung Galaxy S23",
+//         description: "The Samsung Galaxy S23 offers an all-encompassing mobile experience with its advanced AMOLED display, offering vibrant visuals and smooth interactions. Equipped with top-of-the-line fitness tracking features and cutting-edge technology, this phone delivers outstanding performance. With powerful hardware, a sleek design, and long battery life, the S23 is perfect for those who demand the best in mobile innovation.",
+//         image: "/src/assets/samsung_s23phone_image.png",
+//         price: 14999.99,
+//         rating: 4.5,
+//         color: "Multi",
+//         category: "Smartphones"
+//     },
+//     {
+//         id: "81053E2Z7T",
+//         name: "PlayStation 5",
+//         description: "The PlayStation 5 takes gaming to the next level with ultra-HD graphics, a powerful 825GB SSD, and ray tracing technology for realistic visuals. Whether you're into high-action games or immersive storytelling, the PS5 delivers fast loading times, seamless gameplay, and stunning visuals. It's a must-have for any serious gamer looking for the ultimate gaming experience.",
+//         image: "/src/assets/playstation_image.png",
+//         price: 16999.99,
+//         rating: 4.5,
+//         color: "Multi",
+//         category: "Accessories"
+//     },
+//     {
+//         id: "83KRVSDZPD",
+//         name: "MacBook Pro 16",
+//         description: "The MacBook Pro 16, powered by Apple's M2 Pro chip, offers outstanding performance with 16GB RAM and a 512GB SSD. Whether you're editing high-resolution video, developing software, or multitasking with ease, this laptop can handle the toughest tasks. It features a stunning Retina display with True Tone technology, making it a top choice for professionals in creative industries or anyone who demands premium performance in a portable form.",
+//         image: "/src/assets/macbook_image.png",
+//         price: 39999.99,
+//         rating: 4.5,
+//         color: "Multi",
+//         category: "Laptops"
+//     },
+//     {
+//         id: "X6B9ZKI4XM",
+//         name: "Garmin Venu 2",
+//         description: "The Garmin Venu 2 smartwatch blends advanced fitness tracking with sophisticated design, offering a wealth of features such as heart rate monitoring, GPS, and sleep tracking. Built with a 24-hour battery life, this watch is ideal for fitness enthusiasts and anyone looking to enhance their daily lifestyle. With a stunning AMOLED display and customizable watch faces, the Venu 2 combines technology with style seamlessly.",
+//         image: "/src/assets/venu_watch_image.png",
+//         price: 4999.99,
+//         rating: 4.5,
+//         color: "Multi",
+//         category: "Smartwaches"
+//     },
+//     {
+//         id: "VAM5T4ML85",
+//         name: "Canon EOS R5",
+//         description: "The Canon EOS R5 is a game-changing mirrorless camera with a 45MP full-frame sensor, offering ultra-high resolution and the ability to shoot 8K video. Whether you're capturing professional-quality stills or cinematic video footage, this camera delivers exceptional clarity, speed, and color accuracy. With advanced autofocus and in-body stabilization, the R5 is ideal for photographers and videographers alike.",
+//         image: "/src/assets/cannon_camera_image.png",
+//         price: 8999.99,
+//         rating: 4.5,
+//         color: "Multi",
+//         category: "Cameras"
+//     },
+//     {
+//         id: "YI9CXSQAF9",
+//         name: "Apple AirPods Pro 2nd gen",
+//         description: "Apple AirPods Pro (2nd Gen) with MagSafe Case (USB-C) provide excellent sound, active noise cancellation, and a comfortable fit. The USB-C case ensures quick charging, and they pair seamlessly with Apple devices for an effortless audio experience.",
+//         image: "/src/assets/apple_earphone_image.png",
+//         price: 699.99,
+//         rating: 4.5,
+//         color: "Multi",
+//         category: "Earphones"
+//     },
+//     {
+//         id: "RNEVYYCINX",
+//         name: "Sony WF-1000XM5",
+//         description: "Sony WF-1000XM5 true wireless earbuds deliver immersive sound with Hi-Res Audio and advanced noise cancellation technology. Designed for comfort and quality, they provide a stable, snug fit for a secure listening experience. Whether you're working out or traveling, these earbuds will keep you connected with the world around you while enjoying rich, clear sound.",
+//         image: "/src/assets/sony_airbuds_image.png",
+//         price: 699.99,
+//         rating: 4.5,
+//         color: "Multi",
+//         category: "Earphones"
+//     },
+//     {
+//         id: "WMV35DS9X5",
+//         name: "Samsung Projector 4k",
+//         description: "The Samsung 4K Projector offers an immersive cinematic experience with ultra-high-definition visuals and realistic color accuracy. Equipped with a built-in speaker, it delivers rich sound quality to complement its stunning 4K resolution. Perfect for movie nights, gaming, or presentations, this projector is the ultimate choice for creating an at-home theater experience or professional setting.",
+//         image: "/src/assets/projector_image.png",
+//         price: 2499.99,
+//         rating: 4.5,
+//         color: "Multi",
+//         category: "Accessories"
+//     },
+// ]
+
+import asusImg from "../assets/asus_laptop_image.png";
+import boseImg from "../assets/bose_headphone_image.png";
+import samsungS23Img from "../assets/samsung_s23phone_image.png";
+import ps5Img from "../assets/playstation_image.png";
+import macbookImg from "../assets/macbook_image.png";
+import venuWatchImg from "../assets/venu_watch_image.png";
+import canonImg from "../assets/cannon_camera_image.png";
+import airpodsImg from "../assets/apple_earphone_image.png";
+import sonyEarbudsImg from "../assets/sony_airbuds_image.png";
+import projectorImg from "../assets/projector_image.png";
 
 
 export const products = [
     {
         id: "5RKWDRWZ4N",
         name: "ASUS ROG Zephyrus G16",
-        description: "The ASUS ROG Zephyrus G16 gaming laptop is powered by the Intel Core i9 processor and features an RTX 4070 GPU, delivering top-tier gaming and performance. With 16GB of RAM and a 1TB SSD, this laptop is designed for gamers who demand extreme power, speed, and storage. Equipped with a stunning 16-inch display, it's built to handle the most demanding titles and applications with ease.",
-        image: "/src/assets/asus_laptop_image.png",
+        description: "The ASUS ROG Zephyrus G16 gaming laptop is powered by the Intel Core i9 processor and features an RTX 4070 GPU...",
+        image: asusImg,
         price: 34599.99,
         rating: 4.5,
         color: "Multi",
@@ -22,8 +137,8 @@ export const products = [
     {
         id: "BJJFZBBDIA",
         name: "Bose QuietComfort 45",
-        description: "The Bose QuietComfort 45 headphones are engineered for exceptional sound quality and unparalleled noise cancellation. With a 24-hour battery life and comfortable, lightweight design, these headphones deliver premium audio for any environment. Whether on a flight, in the office, or at home, the Bose QC45 blocks out distractions, offering an immersive listening experience.",
-        image: "/src/assets/bose_headphone_image.png",
+        description: "The Bose QuietComfort 45 headphones are engineered for exceptional sound quality...",
+        image: boseImg,
         price: 329.99,
         rating: 4.5,
         color: "Black",
@@ -32,8 +147,8 @@ export const products = [
     {
         id: "MDI7BWBE0T",
         name: "Samsung Galaxy S23",
-        description: "The Samsung Galaxy S23 offers an all-encompassing mobile experience with its advanced AMOLED display, offering vibrant visuals and smooth interactions. Equipped with top-of-the-line fitness tracking features and cutting-edge technology, this phone delivers outstanding performance. With powerful hardware, a sleek design, and long battery life, the S23 is perfect for those who demand the best in mobile innovation.",
-        image: "/src/assets/samsung_s23phone_image.png",
+        description: "The Samsung Galaxy S23 offers an all-encompassing mobile experience...",
+        image: samsungS23Img,
         price: 14999.99,
         rating: 4.5,
         color: "Multi",
@@ -42,8 +157,8 @@ export const products = [
     {
         id: "81053E2Z7T",
         name: "PlayStation 5",
-        description: "The PlayStation 5 takes gaming to the next level with ultra-HD graphics, a powerful 825GB SSD, and ray tracing technology for realistic visuals. Whether you're into high-action games or immersive storytelling, the PS5 delivers fast loading times, seamless gameplay, and stunning visuals. It's a must-have for any serious gamer looking for the ultimate gaming experience.",
-        image: "/src/assets/playstation_image.png",
+        description: "The PlayStation 5 takes gaming to the next level with ultra-HD graphics...",
+        image: ps5Img,
         price: 16999.99,
         rating: 4.5,
         color: "Multi",
@@ -52,8 +167,8 @@ export const products = [
     {
         id: "83KRVSDZPD",
         name: "MacBook Pro 16",
-        description: "The MacBook Pro 16, powered by Apple's M2 Pro chip, offers outstanding performance with 16GB RAM and a 512GB SSD. Whether you're editing high-resolution video, developing software, or multitasking with ease, this laptop can handle the toughest tasks. It features a stunning Retina display with True Tone technology, making it a top choice for professionals in creative industries or anyone who demands premium performance in a portable form.",
-        image: "/src/assets/macbook_image.png",
+        description: "The MacBook Pro 16, powered by Apple's M2 Pro chip...",
+        image: macbookImg,
         price: 39999.99,
         rating: 4.5,
         color: "Multi",
@@ -62,8 +177,8 @@ export const products = [
     {
         id: "X6B9ZKI4XM",
         name: "Garmin Venu 2",
-        description: "The Garmin Venu 2 smartwatch blends advanced fitness tracking with sophisticated design, offering a wealth of features such as heart rate monitoring, GPS, and sleep tracking. Built with a 24-hour battery life, this watch is ideal for fitness enthusiasts and anyone looking to enhance their daily lifestyle. With a stunning AMOLED display and customizable watch faces, the Venu 2 combines technology with style seamlessly.",
-        image: "/src/assets/venu_watch_image.png",
+        description: "The Garmin Venu 2 smartwatch blends advanced fitness tracking...",
+        image: venuWatchImg,
         price: 4999.99,
         rating: 4.5,
         color: "Multi",
@@ -72,8 +187,8 @@ export const products = [
     {
         id: "VAM5T4ML85",
         name: "Canon EOS R5",
-        description: "The Canon EOS R5 is a game-changing mirrorless camera with a 45MP full-frame sensor, offering ultra-high resolution and the ability to shoot 8K video. Whether you're capturing professional-quality stills or cinematic video footage, this camera delivers exceptional clarity, speed, and color accuracy. With advanced autofocus and in-body stabilization, the R5 is ideal for photographers and videographers alike.",
-        image: "/src/assets/cannon_camera_image.png",
+        description: "The Canon EOS R5 is a game-changing mirrorless camera...",
+        image: canonImg,
         price: 8999.99,
         rating: 4.5,
         color: "Multi",
@@ -82,8 +197,8 @@ export const products = [
     {
         id: "YI9CXSQAF9",
         name: "Apple AirPods Pro 2nd gen",
-        description: "Apple AirPods Pro (2nd Gen) with MagSafe Case (USB-C) provide excellent sound, active noise cancellation, and a comfortable fit. The USB-C case ensures quick charging, and they pair seamlessly with Apple devices for an effortless audio experience.",
-        image: "/src/assets/apple_earphone_image.png",
+        description: "Apple AirPods Pro (2nd Gen) with MagSafe Case (USB-C)...",
+        image: airpodsImg,
         price: 699.99,
         rating: 4.5,
         color: "Multi",
@@ -92,8 +207,8 @@ export const products = [
     {
         id: "RNEVYYCINX",
         name: "Sony WF-1000XM5",
-        description: "Sony WF-1000XM5 true wireless earbuds deliver immersive sound with Hi-Res Audio and advanced noise cancellation technology. Designed for comfort and quality, they provide a stable, snug fit for a secure listening experience. Whether you're working out or traveling, these earbuds will keep you connected with the world around you while enjoying rich, clear sound.",
-        image: "/src/assets/sony_airbuds_image.png",
+        description: "Sony WF-1000XM5 true wireless earbuds deliver immersive sound...",
+        image: sonyEarbudsImg,
         price: 699.99,
         rating: 4.5,
         color: "Multi",
@@ -102,11 +217,11 @@ export const products = [
     {
         id: "WMV35DS9X5",
         name: "Samsung Projector 4k",
-        description: "The Samsung 4K Projector offers an immersive cinematic experience with ultra-high-definition visuals and realistic color accuracy. Equipped with a built-in speaker, it delivers rich sound quality to complement its stunning 4K resolution. Perfect for movie nights, gaming, or presentations, this projector is the ultimate choice for creating an at-home theater experience or professional setting.",
-        image: "/src/assets/projector_image.png",
+        description: "The Samsung 4K Projector offers an immersive cinematic experience...",
+        image: projectorImg,
         price: 2499.99,
         rating: 4.5,
         color: "Multi",
         category: "Accessories"
-    },
-]
+    }
+];
